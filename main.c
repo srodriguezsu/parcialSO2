582c2b8337e6576e4dea6f018e719a13a7377aac
srodriguezsu <srodriguezsu@unal.edu.co>
2024-09-12 14:11:01 -0500

    Creo que funciona melo

--- main.c
+++ main.c
@@ -5,11 +5,16 @@
 #define _POSIX_C_SOURCE 200809L
 
 int cont = 0;
-int tamanoPagina = 4096;
+const int tamanoPagina = 4096;
+const int tamanoEntrada = 24;
+const int cantidadEntradas = 5;
 
 void printEntradaTlb(int *tlb, int posicion, int isHit, int isReplaced) {
+
+    int *posicionPtr = tlb+(posicion*tamanoEntrada);
+
     int * inicio = tlb;
-    int * final = tlb+25;
+    int * final = tlb+(tamanoEntrada*cantidadEntradas);
     
     
     printf("TLB desde: %p hasta %p\n", inicio, final);
@@ -19,16 +24,16 @@ void printEntradaTlb(int *tlb, int posicion, int isHit, int isReplaced) {
     } else{
         printf("TLB Miss\n");
     }
-    printf("Pagina: %d\n", *(tlb+(posicion*5) + 1));
-    printf("Desplazamiento: %d\n", *(tlb+(posicion*5) + 2));
+    printf("Pagina: %d\n", *(posicionPtr + 1));
+    printf("Desplazamiento: %d\n", *(posicionPtr + 2));
 
-    char *string_ptr = (char*)tlb + posicion * 5 + (3*sizeof(int));
-    printf("Desplazamiento en Binario: %s\n", string_ptr);
-    // printf("Pagina en Binario: %s\n", (char*) *(tlb+(posicion*5) + 3));
-    // printf("Desplazamiento en Binario: %d\n", *(tlb+(posicion*5) + 4));
+    char *pagBiPtr = (char*)posicionPtr + (3*sizeof(int));
+    char *desBiPtr = (char*)posicionPtr + (3*sizeof(int)) + 21;
+    printf("Pagina en Binario: %s\n", pagBiPtr);
+    printf("Desplazamiento en Binario: %s\n", desBiPtr);
     if (isReplaced > 0)
     {
-        int *reemplazo = tlb+(posicion*5);
+        int *reemplazo = tlb+(posicion*tamanoEntrada);
         printf("Politica de reemplazo %p\n", &reemplazo);
     }
     else{
@@ -38,23 +43,27 @@ void printEntradaTlb(int *tlb, int posicion, int isHit, int isReplaced) {
     return;
 }
 void guardarEntradaTlb(int* tlb, int posicion, int direccion){
-    int a = floor(direccion / tamanoPagina);
-    int b = direccion % tamanoPagina;
-    // char* c = decimalABinario(a);
-    // char* d = decimalABinario(b);
-    
-    char binario[21];
-    decimalABinario(b, binario);
-    
-    *(tlb+(posicion*5)) = direccion; //Direccion
-    *(tlb+(posicion*5) + 1) = a; // Pagina
-    *(tlb+(posicion*5) + 2) = b; // Desplazamiento
 
-    char *string_ptr = (char*)tlb + posicion * 5 + (3*sizeof(int));
+    int *posicionPtr = tlb+(posicion*tamanoEntrada);
 
-    snprintf(string_ptr, 21, binario);
-    // *(tlb+(posicion*5) + 3) = (char*) binario; // Pagina en binario
-    // *(tlb+(posicion*5) + 24) = d; // Desplazamiento en binario
+
+    int pagina = floor(direccion / tamanoPagina);
+    int desplazamiento = direccion % tamanoPagina;
+    char paginaBinario[20];
+    char desplazamientoBinario[20];
+
+    decimalABinario(pagina, paginaBinario);
+    decimalABinario(desplazamiento, desplazamientoBinario);
+
+    *(posicionPtr) = direccion; 
+    *(posicionPtr + 1) = pagina; 
+    *(posicionPtr + 2) = desplazamiento; 
+
+    char *pagBiPtr = (char*)posicionPtr + (3*sizeof(int));
+    char *desBiPtr = (char*)posicionPtr + (3*sizeof(int)) + 21;
+
+    snprintf(pagBiPtr, 21, paginaBinario);
+    snprintf(desBiPtr, 21, desplazamientoBinario);
     return;
 }
 
@@ -74,28 +83,28 @@ int binarioAlDecimal(int numero){
 
 void anadirTLB(int *tlb, int direccion) {
     // Buscar direccion en el tlb
-    for (int i = 0; i<5; i++) {
-            if (direccion == *(tlb+(5*i))) {
+    for (int i = 0; i<cantidadEntradas; i++) {
+            if (direccion == *(tlb+(tamanoEntrada*i))) {
                 // si la encuentra, la imprime
-                printEntradaTlb(tlb, i, 1, 0);
+                printEntradaTlb(tlb, i, 1, 0); // tlb, posicion a imprimir, isHit, !isReplaced
                 return;
             }
          }
     // si la cola esta llena 
-    if (cont == 5) {
+    if (cont == cantidadEntradas) {
         // Mover todos los elementos para sobre escribir el primero
         for (int i = 0; i <= 20; i++) {
-            *(tlb+i) = *(tlb+i+5); 
+            *(tlb+i) = *(tlb+i+tamanoEntrada); 
         }
         // guardar la direccion al final de la cola
-        guardarEntradaTlb(tlb, 4, direccion);
-        printEntradaTlb(tlb, 4, 0, 1);
+        guardarEntradaTlb(tlb, 4, direccion); 
+        printEntradaTlb(tlb, 4, 0, 1);  // tlb, posicion a imprimir, !isHit, isReplaced
     }
 
-    else if (cont<5) {
+    else if (cont<cantidadEntradas) {
         // si la cola no esta llena, añade al final
         guardarEntradaTlb(tlb, cont, direccion);
-        printEntradaTlb(tlb, cont, 0, 0);
+        printEntradaTlb(tlb, cont, 0, 0);  // tlb, posicion a imprimir, !isHit, !isReplaced
         cont++;
         return;
     }
@@ -107,7 +116,7 @@ int main () {
     printf("Ingrese su direccion virtual: ");
     scanf("%s", &input);
     
-    int *tlb =  malloc(285); 
+    int *tlb =  malloc(tamanoEntrada * cantidadEntradas); 
     
 
     while (strcmp(input, "s") != 0) {
