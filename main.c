9a75e1ea4d2b2ae8844eec7493c76b2ba2bad321
srodriguezsu <srodriguezsu@unal.edu.co>
2024-09-12 11:49:37 -0500

    clean code

--- main.c
+++ main.c
@@ -7,6 +7,47 @@
 int cont = 0;
 int tamanoPagina = 4096;
 
+void printEntradaTlb(int *tlb, int posicion, int isHit, int isReplaced) {
+    int * inicio = tlb;
+    int * final = tlb+25;
+    
+    
+    printf("TLB desde: %p hasta %p\n", inicio, final);
+    if (isHit > 0)
+    {
+        printf("TLB Hit\n");
+    } else{
+        printf("TLB Miss\n");
+    }
+    printf("Pagina: %d\n", *(tlb+(posicion*5) + 1));
+    printf("Desplazamiento: %d\n", *(tlb+(posicion*5) + 2));
+    printf("Pagina en Binario: %d\n", *(tlb+(posicion*5) + 3));
+    printf("Desplazamiento en Binario: %d\n", *(tlb+(posicion*5) + 4));
+    if (isReplaced > 0)
+    {
+        int *reemplazo = tlb+(posicion*5);
+        printf("Politica de reemplazo %p\n", &reemplazo);
+    }
+    else{
+        printf("Politica de reemplazo 0x0\n");
+    }
+    
+    return;
+}
+void guardarEntradaTlb(int* tlb, int posicion, int direccion){
+    int a = floor(direccion / tamanoPagina);
+    int b = direccion % tamanoPagina;
+    int c = decimalABinario(a);
+    int d = decimalABinario(b);
+    
+    *(tlb+(posicion*5)) = direccion; //Direccion
+    *(tlb+(posicion*5) + 1) = a; // Pagina
+    *(tlb+(posicion*5) + 2) = b; // Desplazamiento
+    *(tlb+(posicion*5) + 3) = c; // Pagina en binario
+    *(tlb+(posicion*5) + 4) = d; // Desplazamiento en binario
+    return;
+}
+
 int decimalABinario(int n) {
     int binary = 0, base = 1;
     while (n > 0) {
@@ -24,90 +65,33 @@ int binarioAlDecimal(int numero){
 }
 
 
-void anadirTLB(int *tlb, int x) {
-
-
+void anadirTLB(int *tlb, int direccion) {
+    // Buscar direccion en el tlb
     for (int i = 0; i<5; i++) {
-
-            if (x == *(tlb+(5*i))) {
-
-                int * inicio = tlb;
-                int * final = tlb+25;
-                printf("TLB desde: %p hasta %p\n", inicio, final);
-                printf("HIT \n");
-                printf("Pagina: %d\n", *(tlb+(i*5) + 1));
-                printf("Desplazamiento: %d\n", *(tlb+(i*5) + 2));
-                printf("Pagina en Binario: %d\n", *(tlb+(i*5) + 3));
-                printf("Desplazamiento en Binario: %d\n", *(tlb+(i*5) + 4));
-                printf("Politica de reemplazo: 0x0\n");
+            if (direccion == *(tlb+(5*i))) {
+                // si la encuentra, la imprime
+                printEntradaTlb(tlb, i, 1, 0);
                 return;
             }
-
          }
-
+    // si la cola esta llena 
     if (cont == 5) {
-        
-
+        // Mover todos los elementos para sobre escribir el primero
         for (int i = 0; i <= 20; i++) {
             *(tlb+i) = *(tlb+i+5); 
         }
-        
-         int a = floor(x / tamanoPagina);
-         int b = x % tamanoPagina;
-         int c = decimalABinario(a);
-         int d = decimalABinario(b);
-
-         *(tlb+20) = x;
-         *(tlb+21) = a; //Pagina
-         *(tlb+22) = b; // Desplazamiento
-         *(tlb+23) = c; // Pagina en binario
-         *(tlb+24) = d; // Desplazamiento en binario
-         
-
-         int * inicio = tlb;
-         int * final = tlb+25;
-         printf("TLB desde: %p hasta %p\n", inicio, final);
-         printf("MISS \n"); 
-         printf("Pagina: %d\n", *(tlb+21));
-         printf("Desplazamiento: %d\n", *(tlb+22));
-         printf("Pagina en Binario: %d\n", *(tlb+23));
-         printf("Desplazamiento en Binario: %d\n", *(tlb+24));
-
-         int *reemplazo = tlb+20;
-         printf("Politica de reemplazo %p\n", &reemplazo);
-
-
+        // guardar la direccion al final de la cola
+        guardarEntradaTlb(tlb, 4, direccion);
+        printEntradaTlb(tlb, 4, 0, 1);
     }
 
     else if (cont<5) {
-
-        int a = floor(x / tamanoPagina);
-        int b = x % tamanoPagina;
-        int c = decimalABinario(a);
-        int d = decimalABinario(b);
-
-        *(tlb+(cont*5)) = x; //Direccion
-        *(tlb+(cont*5) + 1) = a; // Pagina
-        *(tlb+(cont*5) + 2) = b; // Desplazamiento
-        *(tlb+(cont*5) + 3) = c; // Pagina en binario
-        *(tlb+(cont*5) + 4) = d; // Desplazamiento en binario
-
-        printf("MISS \n"); 
-        int * inicio = tlb;
-        int * final = tlb+25;
-        printf("TLB desde: %p hasta %p\n", inicio, final);
-        printf("Pagina: %d\n", *(tlb+(cont*5) + 1));
-        printf("Desplazamiento: %d\n", *(tlb+(cont*5) + 2));
-        printf("Pagina en Binario: %d\n", *(tlb+(cont*5) + 3));
-        printf("Desplazamiento en Binario: %d\n", *(tlb+(cont*5) + 4));
-        printf("Politica de reemplazo: 0x0\n");
-
+        // si la cola no esta llena, añade al final
+        guardarEntradaTlb(tlb, cont, direccion);
+        printEntradaTlb(tlb, cont, 0, 0);
         cont++;
-
         return;
-
     }
-
 }
 
 
@@ -130,15 +114,6 @@ int main () {
         
         anadirTLB(tlb, direccion);
         
-
-
-
-
-        
-        
-        
-        
-         
         clock_gettime(CLOCK_REALTIME, &tiempoFinal);
 
 
@@ -154,12 +129,13 @@ int main () {
         elapsed = seconds + nanoseconds*1e-9;
 
         // Print the elapsed time
-        printf("Elapsed time: %.9f seconds\n", elapsed);
+        printf("Tiempo: %.9f segundos\n", elapsed);
 
         
         printf("\nIngrese su direccion virtual: ");
         scanf("%s", &input);      
-        //sebas es un gilipollas 
+        
     }
     free(tlb);
+    printf("\nGood bye!\n");
 }
